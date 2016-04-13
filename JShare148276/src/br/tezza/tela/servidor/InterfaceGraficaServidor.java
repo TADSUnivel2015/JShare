f87f6020edbfe180420aa63c555b6fc47d504dcf
Alex Tezza <Alex Tezza@DESKTOP-F4OTEBU>
2016-04-13 08:11:53 -0300

    Criando a interface do servidor.

---
+++ JShare148276/src/br/tezza/tela/servidor/InterfaceGraficaServidor.java
@@ -0,0 +1,104 @@
+package br.tezza.tela.servidor;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.border.TitledBorder;
+import javax.swing.JComboBox;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class InterfaceGraficaServidor extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtPorta;
+	
+	private int flag = 0;
+	private JButton btnPararServidor;
+	private JButton btnIniciarServidor;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					InterfaceGraficaServidor frame = new InterfaceGraficaServidor();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public InterfaceGraficaServidor() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(5, 5, 424, 28);
+		contentPane.add(panel);
+		
+		JLabel lblServidorDeBusca = new JLabel("Servidor de busca de arquivos");
+		lblServidorDeBusca.setFont(new Font("Verdana", Font.BOLD, 14));
+		panel.add(lblServidorDeBusca);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(5, 44, 424, 33);
+		contentPane.add(panel_1);
+		
+		JLabel lblIp = new JLabel("IP:");
+		lblIp.setVerticalAlignment(SwingConstants.BOTTOM);
+		panel_1.add(lblIp);
+		
+		JComboBox cbxIp = new JComboBox();
+		panel_1.add(cbxIp);
+		
+		JLabel lblPorta = new JLabel("Porta: ");
+		panel_1.add(lblPorta);
+		
+		txtPorta = new JTextField();
+		panel_1.add(txtPorta);
+		txtPorta.setColumns(10);
+		
+		btnIniciarServidor = new JButton("Iniciar Servidor");
+		btnIniciarServidor.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				btnIniciarServidor.setVisible(false);
+				btnPararServidor.setVisible(true);
+			}
+		});
+		panel_1.add(btnIniciarServidor);
+		
+		btnPararServidor = new JButton("Parar Servidor");
+		btnPararServidor.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				btnPararServidor.setVisible(false);
+				btnIniciarServidor.setVisible(true);
+				
+			}
+		});
+		panel_1.add(btnPararServidor);
+	}
+}

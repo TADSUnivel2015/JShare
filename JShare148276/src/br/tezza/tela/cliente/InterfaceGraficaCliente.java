5f0533f812f71864cee21a1062772c9e0787d91a
Tezza <Tezza@Tezza-PC>
2016-05-01 13:24:33 -0300

    Criação dos métodos 'desconectarUsuario' e 'configuraBotoes' na classe 'InterfaceGraficaCliente'.

--- JShare148276/src/br/tezza/tela/cliente/InterfaceGraficaCliente.java
+++ JShare148276/src/br/tezza/tela/cliente/InterfaceGraficaCliente.java
@@ -5,10 +5,12 @@ import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.rmi.Naming;
+import java.rmi.NoSuchObjectException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.Map;
@@ -42,7 +44,7 @@ public class InterfaceGraficaCliente extends JFrame implements IServer{
 	private JTable tabelaResultadoBusca;
 	private JTextField txtIpServidor;
 	private JTextField txtMinhaPorta;
-	
+
 	private ListaIP listaIP = new ListaIP();
 
 	/**
@@ -72,105 +74,116 @@ public class InterfaceGraficaCliente extends JFrame implements IServer{
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JPanel panel = new JPanel();
 		panel.setBounds(5, 5, 526, 206);
 		contentPane.add(panel);
 		panel.setLayout(null);
-		
+
 		JLabel lblNome = new JLabel("Nome:");
 		lblNome.setBounds(32, 9, 39, 14);
 		panel.add(lblNome);
-		
+
 		txtNomeUsuario = new JTextField();
 		txtNomeUsuario.setBounds(81, 6, 312, 20);
 		panel.add(txtNomeUsuario);
 		txtNomeUsuario.setColumns(10);
-		
-		JButton btnDisponibilizarMeusArquivos = new JButton("Disponibilizar meus arquivos");
+
+		btnDisponibilizarMeusArquivos = new JButton("Disponibilizar meus arquivos");
 		btnDisponibilizarMeusArquivos.setBounds(10, 110, 278, 23);
 		panel.add(btnDisponibilizarMeusArquivos);
-		
-		JButton btnConectar = new JButton("Conectar");
+
+		btnConectar = new JButton("Conectar");
 		btnConectar.setBounds(298, 110, 95, 23);
 		panel.add(btnConectar);
-		
+
 		txtBuscaArquivo = new JTextField();
 		txtBuscaArquivo.setToolTipText("");
 		txtBuscaArquivo.setColumns(10);
 		txtBuscaArquivo.setBounds(10, 173, 278, 20);
 		panel.add(txtBuscaArquivo);
-		
-		JButton btnBuscarArquivo = new JButton("Buscar Arquivo");
+
+		btnBuscarArquivo = new JButton("Buscar Arquivo");
 		btnBuscarArquivo.setBounds(298, 172, 218, 23);
 		panel.add(btnBuscarArquivo);
-		
+
 		JLabel lblIp = new JLabel("IP Servidor:");
 		lblIp.setBounds(10, 40, 89, 14);
 		panel.add(lblIp);
-		
+
 		txtIpServidor = new JTextField();
 		txtIpServidor.setText("127.0.0.1");
 		txtIpServidor.setBounds(81, 37, 103, 20);
 		panel.add(txtIpServidor);
 		txtIpServidor.setColumns(10);
-		
+
 		JLabel lblPorta = new JLabel("Porta Servidor:");
 		lblPorta.setBounds(202, 43, 103, 14);
 		panel.add(lblPorta);
-		
+
 		txtMinhaPorta = new JTextField();
 		txtMinhaPorta.setText("1415");
 		txtMinhaPorta.setBounds(298, 79, 95, 20);
 		panel.add(txtMinhaPorta);
 		txtMinhaPorta.setColumns(10);
-		
-		JButton btnDesconectar = new JButton("Desconectar");
+
+		btnDesconectar = new JButton("Desconectar");
 		btnDesconectar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 			}
 		});
 		btnDesconectar.setBounds(403, 110, 113, 23);
 		panel.add(btnDesconectar);
-		
+
 		JLabel lblSeuIp = new JLabel("Seu IP:");
 		lblSeuIp.setBounds(32, 81, 46, 14);
 		panel.add(lblSeuIp);
-		
+
 		cbxMeuIP = new JComboBox();
 		cbxMeuIP.setBounds(81, 79, 103, 20);
 		panel.add(cbxMeuIP);
-		
+
 		List<String> lista = listaIP.buscaIp();
 		cbxMeuIP.setModel(new DefaultComboBoxModel<String>(new Vector<String>(lista)));
 		cbxMeuIP.setSelectedIndex(0);
-		
+
 		JLabel lblPortaDisponvel = new JLabel("Porta Dispon\u00EDvel:");
 		lblPortaDisponvel.setBounds(194, 82, 103, 14);
 		panel.add(lblPortaDisponvel);
-		
+
 		txtPortaServidor = new JTextField();
 		txtPortaServidor.setText("1818");
 		txtPortaServidor.setBounds(298, 37, 95, 20);
 		panel.add(txtPortaServidor);
 		txtPortaServidor.setColumns(10);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(5, 222, 526, 177);
 		contentPane.add(scrollPane);
-		
+
 		tabelaResultadoBusca = new JTable();
 		scrollPane.setViewportView(tabelaResultadoBusca);
-		
+
 		btnConectar.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				conectar();
 			}
 		});
+
+		btnDesconectar.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				desconectarUsuario();
+			}
+
+		});
+
 	}
-	
-	
+
+
+
 	/**
 	 * ===================================================================================================
 	 * 
@@ -182,13 +195,13 @@ public class InterfaceGraficaCliente extends JFrame implements IServer{
 	@Override
 	public void registrarCliente(Cliente c) throws RemoteException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void publicarListaArquivos(Cliente c, List<Arquivo> lista) throws RemoteException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -206,9 +219,9 @@ public class InterfaceGraficaCliente extends JFrame implements IServer{
 	@Override
 	public void desconectar(Cliente c) throws RemoteException {
 		// TODO,. Auto-generated method stub
-		
+
 	}
-	
+
 	/**
 	 * ===================================================================================================
 	 * 
@@ -216,22 +229,26 @@ public class InterfaceGraficaCliente extends JFrame implements IServer{
 	 * 
 	 * ===================================================================================================
 	 */
-	
-	
+
+
 	// Formato de data, utilizando a classe DateFormat.
 	private SimpleDateFormat dateFormat = new DateFormat().formatoData();
-	
+
 	private IServer iServer;
-	
+
 	private Registry registry;
-	
+
 	private InterfaceGraficaServidor servidor;
-	
+
 	private String meuNome;
 	private JTextField txtPortaServidor;
 	private JComboBox cbxMeuIP;
-	
-	
+	private JButton btnConectar;
+	private JButton btnDisponibilizarMeusArquivos;
+	private JButton btnDesconectar;
+	private JButton btnBuscarArquivo;
+
+
 	/**
 	 * ===================================================================================================
 	 * 
@@ -239,21 +256,21 @@ public class InterfaceGraficaCliente extends JFrame implements IServer{
 	 * 
 	 * ===================================================================================================
 	 */
-	
+
 	protected void conectar() {
-		
+
 		meuNome = txtNomeUsuario.getText().trim();
 		if (meuNome.length() == 0) {
 			JOptionPane.showMessageDialog(this, "Informe seu nome!");
 			return;
 		}
-		
+
 		String host = txtIpServidor.getText().trim();
 		if (!host.matches("[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}")) {
 			JOptionPane.showMessageDialog(this, "Endereço IP é inválido, por gentileza, informe outro.");
 			return;
 		}
-		
+
 		String strPorta = txtMinhaPorta.getText().trim();
 		if (!strPorta.matches("[0-9]+") || strPorta.length() > 5){
 			JOptionPane.showMessageDialog(this, "O número da porta deve ser um valor de no máximo 5 digitos!");
@@ -265,24 +282,24 @@ public class InterfaceGraficaCliente extends JFrame implements IServer{
 			JOptionPane.showMessageDialog(this, "O número da porta deve estar entre 1024 e 65535");
 			return;
 		}
-		
+
 		//Iniciando objetos para conexão.
 		try {
-			
+
 			registry = LocateRegistry.getRegistry(host, 1818);
-			
+
 			iServer = (IServer) registry.lookup(IServer.NOME_SERVICO);
-			
+
 			Cliente cliente = new Cliente();
-			
+
 			cliente.setNome(txtNomeUsuario.getText());
 			cliente.setIp(cbxMeuIP.getSelectedItem().toString());
 			cliente.setPorta(Integer.parseInt(txtMinhaPorta.getText()));
-			
+
 			iServer.registrarCliente(cliente);
-			
+
 			JOptionPane.showMessageDialog(this, "conectado!");
-			
+
 		} catch (RemoteException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -291,7 +308,39 @@ public class InterfaceGraficaCliente extends JFrame implements IServer{
 			e.printStackTrace();
 		}
 		
+		configuraBotoes(false);
+	}
+	
+	protected void desconectarUsuario() {
+	
+		try {
+			
+			UnicastRemoteObject.unexportObject(this, true);
+		} catch (NoSuchObjectException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
+		JOptionPane.showMessageDialog(this, "Você se desconectou do Servidor...");
 		
+		configuraBotoes(true);		
+		
+	}
+
+	private void configuraBotoes(Boolean status) {
+		
+		btnConectar.setEnabled(status);
+		btnDisponibilizarMeusArquivos.setEnabled(status);
+		
+		btnBuscarArquivo.setEnabled(!status);
+		btnDesconectar.setEnabled(!status);
+		
+		txtNomeUsuario.setEnabled(status);
+		txtIpServidor.setEnabled(status);
+		txtPortaServidor.setEnabled(status);
+		cbxMeuIP.setEnabled(status);
+		txtMinhaPorta.setEnabled(status);
+		
+		txtBuscaArquivo.setEnabled(!status);
 	}
 }
